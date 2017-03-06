1e5153039a5e0576484593f94dd1d01af4a4786a
Linkcool2006 <alloatous2@gmail.com>
2017-03-06 15:30:30 -0500

    Added soundmenu

---
+++ soundmenu/lua/autorun/client/cl_soundmenu.lua
@@ -0,0 +1,121 @@
+sm = nil
+Color1 = Color(39,39,39)
+Color2 = Color(199,0,0)
+
+function sm_toggle()
+	if !IsValid(sm) then
+		sm = vgui.Create("DFrame")
+		sm:SetSize(350, 400)
+		sm:SetPos((ScrW()/2-sm:GetWide()/2), (ScrH()/2-sm:GetTall()))
+		sm:SetTitle("")
+		sm:ShowCloseButton(false)
+		sm.Paint = function()
+			draw.RoundedBox(0,0,0,sm:GetWide(),sm:GetTall(),Color(59,59,59,245))
+			draw.RoundedBox(0,0,0,sm:GetWide(),25,Color1)
+			draw.SimpleText(">Sound Menu<","DermaDefault",sm:GetWide()/2,5,Color2,TEXT_ALIGN_CENTER)
+		end
+
+		sm:Show()
+		sm:MakePopup()
+		sm:SetKeyboardInputEnabled(false)
+		sm:SetDraggable(false)
+		sm:Center()
+		sm:SetBackgroundBlur(true)
+
+		--Sound files here are NOT USED, Change them in the SERVER SIDED Table!
+		local says = {
+			['a'] = {"049_3.wav","Oh my..."},
+			['b'] = {"049_4.wav","You are not a doctor."},
+			['c'] = {"049_5.wav","I sense the disease in you."},
+			['d'] = {"049_6.wav","I am the cure."},
+			['e'] = {"049_7.wav","Do not be afraid..."},
+			['f'] = {"049_8.wav","Stop resisting..."},
+			['g'] = {"049_alert_1.wav","Hello."},
+			['h'] = {"049_alert_2.wav","Greetings."},
+			['i'] = {"049_9.wav","*Singing*"},
+			['j'] = {"049_10.wav","Oh my...(2)"},
+		}
+
+		local temp = 30
+
+		for k,v in pairs(says) do
+			local b1 = vgui.Create("DButton", sm)
+			b1:SetSize(sm:GetWide()-10, 30)
+			b1:SetPos(5, temp)
+			b1:SetText(v[2])
+			b1.Paint = function()
+				draw.RoundedBox(0,0,0,b1:GetWide(),b1:GetTall(),Color1)
+			end
+			b1.DoClick = function()
+				net.Start("sm_emitsound")
+					--net.WriteString(v[1])
+					net.WriteString(k) --send the ID and not the actual sound!
+				net.SendToServer()
+			end
+			b1.OnCursorEntered = function()
+				b1.Paint = function()
+					draw.RoundedBox(0,0,0,b1:GetWide(),b1:GetTall(),Color2)
+				end
+			end
+			b1.OnCursorExited = function()
+				b1.Paint = function()
+					draw.RoundedBox(0,0,0,b1:GetWide(),b1:GetTall(),Color1)
+				end
+			end
+			b1.OnMousePressed = function()
+				b1.Paint = function()
+					draw.RoundedBox(0,0,0,b1:GetWide(),b1:GetTall(),Color1)
+				end
+			end
+			b1.OnMouseReleased = function()
+				b1.Paint = function()
+					draw.RoundedBox(0,0,0,b1:GetWide(),b1:GetTall(),Color2)
+				end
+				b1:DoClick()
+			end
+			temp = temp + 31
+		end
+		
+		--Close button 
+		local bClose = vgui.Create("DButton", sm)
+		bClose:SetSize(sm:GetWide()-10, 30)
+		bClose:SetPos(5, temp) --at the bottom
+		bClose:SetText("Close")
+		bClose.Paint = function()
+			draw.RoundedBox(0,0,0,bClose:GetWide(),bClose:GetTall(),Color1)
+		end
+		bClose.DoClick = function()
+			sm:Remove()
+		end
+		bClose.OnCursorEntered = function()
+			bClose.Paint = function()
+				draw.RoundedBox(0,0,0,bClose:GetWide(),bClose:GetTall(),Color2)
+			end
+		end
+		bClose.OnCursorExited = function()
+			bClose.Paint = function()
+				draw.RoundedBox(0,0,0,bClose:GetWide(),bClose:GetTall(),Color1)
+			end
+		end
+		bClose.OnMousePressed = function()
+			bClose.Paint = function()
+				draw.RoundedBox(0,0,0,bClose:GetWide(),bClose:GetTall(),Color1)
+			end
+		end
+		bClose.OnMouseReleased = function()
+			bClose.Paint = function()
+				draw.RoundedBox(0,0,0,bClose:GetWide(),bClose:GetTall(),Color2)
+			end
+			bClose:DoClick()
+		end
+		
+	else
+		sm:Remove()
+	end
+end
+
+hook.Add( "Tick", "SM_ACTION", function()
+	if LocalPlayer():GetNClass() != ROLE_SCP049 then return end
+	if LocalPlayer():KeyPressed( IN_ZOOM ) then sm_toggle() end
+	if LocalPlayer():KeyReleased( IN_ZOOM ) then sm_toggle() end
+end )
---
+++ soundmenu/lua/autorun/server/sv_soundmenu.lua
@@ -0,0 +1,45 @@
+util.AddNetworkString("sm_emitsound")
+
+net.Receive("sm_emitsound",function(ln, ply)
+	--Link2006 fixed the exploit that allowed anyone to play any sound ._.
+	local says = {
+		['a'] = {"049_3.wav","Oh my..."},
+		['b'] = {"049_4.wav","You are not a doctor."},
+		['c'] = {"049_5.wav","I sense the disease in you."},
+		['d'] = {"049_6.wav","I am the cure."},
+		['e'] = {"049_7.wav","Do not be afraid..."},
+		['f'] = {"049_8.wav","Stop resisting..."},
+		['g'] = {"049_alert_1.wav","Hello."},
+		['h'] = {"049_alert_2.wav","Greetings."},
+		['i'] = {"049_9.wav","*Singing*"},
+		['j'] = {"049_10.wav","Oh my...(2)"},
+	}
+
+		
+	if ply:GetNClass() != ROLE_SCP049 then return end
+	if ply:GetNWBool("sm_emitallowed",true) == true then ply:SetNWBool("sm_emitallowed",true) end
+	if ply:GetNWBool("sm_emitallowed") == false then return end
+	local SndKey = net.ReadString()
+	--Makes sure that we don't try to play a nil sound :)
+	if says[SndKey] == nil then SndKey = 'g' end 
+	
+	local Snd = says[SndKey][1] --The sound file in the table,with the key that the client sent. :) 
+	
+	ply:EmitSound( Sound(Snd), 100, 100, 1, CHAN_AUTO )
+	ply:SetNWBool("sm_emitallowed",false)
+
+	timer.Create("sm"..math.random(30100,30500),5,1,function()
+		ply:SetNWBool("sm_emitallowed",true)
+	end)
+end)
+
+resource.AddFile("sound/049_3.wav")
+resource.AddFile("sound/049_4.wav")
+resource.AddFile("sound/049_5.wav")
+resource.AddFile("sound/049_6.wav")
+resource.AddFile("sound/049_7.wav")
+resource.AddFile("sound/049_8.wav")
+resource.AddFile("sound/049_alert_1.wav")
+resource.AddFile("sound/049_alert_2.wav")
+resource.AddFile("sound/049_9.wav")
+resource.AddFile("sound/049_10.wav")
