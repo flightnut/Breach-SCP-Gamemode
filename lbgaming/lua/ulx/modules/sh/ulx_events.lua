a4b9125351f93f54686648e9cc1580dfbc688015
Link2006 <alloatous2@hotmail.com>
2017-04-25 20:51:16 -0400

    Added Hide N Seek event to ulx_events, (Ripper) Updated MTFMenu's size, added  SCP860 model

--- lbgaming/lua/ulx/modules/sh/ulx_events.lua
+++ lbgaming/lua/ulx/modules/sh/ulx_events.lua
@@ -1,4 +1,7 @@
 
+Rewards_SCI = (30*1000) --30 K Regular points
+Rewards_SCP = (15*1000) --15 K Regular points
+HNS_MaxSCP = 5 --5 Seekers
 
 ------------------------------------
 ---	Stuff For Events by Link2006 ---
@@ -174,6 +177,140 @@ local eventstart = ulx.command( "Link2006", "ulx eventstart", ulx.eventstart, "!
 eventstart:defaultAccess( ULib.ACCESS_SUPERADMIN )
 eventstart:help( "Destroys the timers to let a customround take place (Requires br_roundrestart_cl access)" )
 
+function ulx.hidenseek(calling_ply)
+	if SERVER then
+		local AllPlayers = {}
+		for k,v in pairs(player.GetAll()) do
+			if v.ActivePlayer then
+				table.insert(AllPlayers,v)
+			end
+		end
+		if #AllPlayers < 16 then
+			ULib.tsayError(calling_ply,"ERROR: There is not enough active players to start Hide & Seek (16+ required)")
+			AllPlayers = nil
+			return
+		end
+		for i=1,HNS_MaxSCP do
+			timer.Remove("HideNSeek_SetWep_"..i)
+		end
+		for k,v in pairs(player.GetAll()) do
+			v:Freeze(false)
+			v:GodDisable()
+		end
+
+		for i=1,HNS_MaxSCP do
+			MsgC(Color(137,207,240),"Setting seeker: "..tostring(i)..'\n')
+			local seekSCP = table.Random(AllPlayers)
+			seekSCP:SetSCP035()
+			seekSCP:GodEnable()
+			seekSCP:SetNoCollideWithTeammates(true)
+			seekSCP:Freeze(true)
+			timer.Simple(0.25,function()
+				ULib.tsayColor(seekSCP,true,team.GetColor(TEAM_SCP),"[Hide & Seek] You're a seeker, you'll be unfrozen in 45 seconds...")
+			end)
+			--timer.Simple(10,function() --10 seconds after they're spawned.
+			timer.Create("HideNSeek_SetWep_"..i,10,1,function()
+				seekSCP:StripWeapons()
+				seekSCP:Give('weapon_mtf_p90')
+				seekSCP:GiveAmmo(100000,'SMG1') --fuck this,100k :P
+				seekSCP:Give('keycard_level5')
+			end)
+			MsgC(Color(137,207,240),"New Seeker: "..tostring(seekSCP:Nick())..'\n')
+			table.RemoveByValue(AllPlayers,seekSCP)
+		end
+
+		--Teleport them somewhere outside the play area
+		--	OR just freeze them in spawn?
+		timer.Remove('HideNSeek_Unfreeze')
+	    timer.Remove('BreachRespawn_Spectators') --The Spectator Respawn Timer
+	    timer.Remove('BreachRespawn_Spectators_Init') --The timer that checks if we can start the Spectator Respawn timer.
+		timer.Remove('HideNSeek_RestartRound')
+
+		timer.Create("HideNSeek_Unfreeze",45,1,function()
+			for k,v in pairs(team.GetPlayers(TEAM_SCP)) do
+				v:Freeze(false)
+			end
+			ULib.tsayColor(nil,true,Color(0,255,0),"[Hide & Seek] Seekers are now free!")
+		end)
+
+		for k,v in pairs(AllPlayers) do
+			v:SetScientist()
+			v:SetPos(table.Random(SPAWN_CLASSD))
+			timer.Simple(0.25,function()
+				ULib.tsayColor(v,true,team.GetColor(TEAM_SCI),"[Hide & Seek] Seekers will be unfrozen in 45 seconds, hide!")
+				ULib.tsayColor(v,true,Color(255,0,0),"[Hide & Seek] Please note, you cannot hide in the pocket dimension!")
+			end)
+		end
+
+		function HideNSeek_Event(ply)
+			if team.NumPlayers(TEAM_SCI) <= 10 then
+
+				for i=1,HNS_MaxSCP do
+					timer.Remove("HideNSeek_SetWep_"..i)
+				end
+				hook.Remove('PlayerDeath','HideNSeek_Counter1')
+				hook.Remove('PlayerDisconnected','HideNSeek_Counter2')
+				hook.Remove('PlayerSpawn','HideNSeek_Counter3')
+				timer.Remove('HideNSeek_Unfreeze')
+				timer.Remove('HideNSeek_RestartRound')
+
+				ULib.tsayColor(nil,true,Color(255,0,0),"The event is over!",Color(0,255,0)," Winners received "..Rewards_SCI.." points and Seekers got "..Rewards_SCP.."!")
+				ULib.tsayColor(nil,true,Color(0,255,0),"Congratulations!")
+
+				MsgC(Color(137,207,240),"========================================\n")
+				MsgC(Color(137,207,240),"==  WINNERS OF THE HIDE & SEEK EVENT  ==\n")
+				for k,v in pairs(team.GetPlayers(TEAM_SCI)) do
+					MsgC(Color(137,207,240),v:Nick()..'\n')
+					v:PS2_AddStandardPoints(Rewards_SCI,"Winning Hide & Seek event",true)
+				end
+				for k,v in pairs(team.GetPlayers(TEAM_SCP)) do
+					v:PS2_AddStandardPoints(Rewards_SCP,"Being an SCP in Hide & Seek event",true)
+				end
+
+				MsgC(Color(137,207,240),'== Sending the list to online admins! ==\n')
+				for _,pAdmin in pairs(player.GetAll()) do
+					if pAdmin:IsAdmin() then
+						pAdmin:ChatPrint("(To Admins) Here's the list of players who won the event:")
+						for k,v in pairs(team.GetPlayers(TEAM_SCI)) do
+							pAdmin:ChatPrint(v:Nick())
+						end
+					end
+				end
+				MsgC(Color(137,207,240),"========================================\n")
+				HideNSeek_Event = nil --Delete our function, we dont need it anymore. :)
+				RunConsoleCommand("ulx","nuke")
+			end
+		end
+		hook.Add('PlayerDeath','HideNSeek_Counter1',HideNSeek_Event)
+		hook.Add('PlayerDisconnected','HideNSeek_Counter2',HideNSeek_Event)
+		hook.Add('PlayerSpawn','HideNSeek_Counter3',HideNSeek_Event)
+		hook.Add('PreCleanupMap','ResetHideNSeek',function()
+			print("<!> MAP HAS BEEN RESET, ABORTING HIDE & SEEK EVENT <!> ")
+			for k,v in pairs(player.GetAll()) do
+				v:GodDisable()
+				--Other stuff that breaks when map resets goes here
+			end
+			hook.Remove('PlayerDeath','HideNSeek_Counter1')
+			hook.Remove('PlayerDisconnected','HideNSeek_Counter2')
+			hook.Remove('PlayerSpawn','HideNSeek_Counter3')
+			timer.Remove('HideNSeek_Unfreeze')
+		    timer.Remove('BreachRespawn_Spectators') --The Spectator Respawn Timer
+		    timer.Remove('BreachRespawn_Spectators_Init') --The timer that checks if we can start the Spectator Respawn timer.
+			timer.Remove('HideNSeek_RestartRound')
+			for i=1,HNS_MaxSCP do
+				timer.Remove("HideNSeek_SetWep_"..i)
+			end
+		end )
+		--HideNSeek_Event(nil) --No player entity are needed but w/e, this call only makes sure you can even start that event ;)
+	else
+		print("ULX Command for HideNSeek event sent...") --?
+	end
+
+	ulx.fancyLogAdmin( calling_ply, "#A started an event: Hide & Seek" )
+end
+local hidenseek = ulx.command("Link2006","ulx hidenseek",ulx.hidenseek,"!hidenseek")
+hidenseek:defaultAccess(ULib.ACCESS_SUPERADMIN)
+hidenseek:help("Spawns everyone into an Hide & Seek event (except spectators)")
 
 --[[
 -- This is all the other commands i have to do
