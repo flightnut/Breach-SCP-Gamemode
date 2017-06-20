80dc841dacb1b94adc3c80a64706e7be41f294ff
Link2006 <alloatous2@hotmail.com>
2017-06-20 03:52:31 -0400

    Nerfed new katana's gatehealth damage, entity cannot take damage in pre-round anymore.

--- lbgaming/lua/autorun/server/sv_gatehealth.lua
+++ lbgaming/lua/autorun/server/sv_gatehealth.lua
@@ -1,3 +1,4 @@
+print("[GateHealth] Loading...")
 gateHealth = CreateConVar('br_gatehealth','10000',{FCVAR_SERVER_CAN_EXECUTE,FCVAR_REPLICATED},"Sets Gate A/B's Health")
 
 function GateHealth_Init()
@@ -18,13 +19,28 @@ hook.Add('InitPostEntity','GateHealth.Init',GateHealth_Init)
 
 --Hook the entity damages...
 hook.Add('EntityTakeDamage','GateHealth.EntityTakeDamage',function(prop,dmginfo)
+	if preparing then 
+		if dmginfo then 
+			if IsValid(dmginfo:GetAttacker()) then 
+				if dmginfo:GetAttacker():IsPlayer() then 
+					dmginfo:GetAttacker():PrintMessage(HUD_PRINTCENTER,"Please wait for the round to start.")
+				end 
+			end 
+		end 
+		return true --Fuck off katana :( 
+	end 
+	
     if prop:GetClass() == 'prop_dynamic' then
         if string.lower(prop:GetModel()) == 'models/foundation/containment/door01.mdl' then
             --The model seems to match a gate a model
             if IsValid(prop:GetParent()) then --Does it have a parent?
                 if prop:Health() >= 0 then
                     if prop:GetParent():GetClass() == 'func_door' then --Is it a door?
-                        prop:SetHealth(prop:Health() - dmginfo:GetDamage()) --Lower this by that much health
+						if dmginfo:GetAttacker():GetActiveWeapon():GetClass() == "tfa_l4d2_kfkat" then -- new weapon then 
+							prop:SetHealth(prop:Health() - math.random(1,5)) --Lower this by that much health
+						else
+							prop:SetHealth(prop:Health() - dmginfo:GetDamage()) --Lower this by that much health
+						end
                         if prop:Health() <= 0 then --Yes it's Gate A or B, Destroy it.
                             prop:EmitSound(Sound('Metal_Box.Break'))
                             prop:GetParent():Fire('Open') --Force it to open.
@@ -48,3 +64,5 @@ hook.Add('EntityTakeDamage','GateHealth.EntityTakeDamage',function(prop,dmginfo)
         end
     end
 end)
+
+print("[GateHealth] Ready.")
