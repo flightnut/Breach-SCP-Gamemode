3e756e55e8f8378d9c95b25d97409928d8727898
Ripper <flightnut@users.noreply.github.com>
2017-06-20 03:42:55 -0400

    Update shared.lua

--- lbgaming_weapons/lua/weapons/tfa_l4d2_kfkat/shared.lua
+++ lbgaming_weapons/lua/weapons/tfa_l4d2_kfkat/shared.lua
@@ -1,11 +1,6 @@
-if( SERVER ) then
-	AddCSLuaFile( "weapon_katana.lua" )
-	resource.AddFile("models/weapons/w_katana.mdl");
-	resource.AddFile("models/weapons/v_katana.mdl");
-	resource.AddFile("materials/models/weapons/v_katana/katana_normal.vtf");
-	resource.AddFile("materials/models/weapons/v_katana/katana.vtf");
-	resource.AddFile("materials/models/weapons/v_katana/katana.vmt");
-end
+SWEP.Base = "tfa_melee_base"
+SWEP.Category = "SCP Weapons"
+SWEP.PrintName = "SCP-076 Katana"
 
 if( CLIENT ) then
 	SWEP.PrintName = "SCP-076 Katana"
@@ -15,112 +10,104 @@ if( CLIENT ) then
 	SWEP.DrawCrosshair = true
 end
 
-SWEP.Author						= "Vorius"
-SWEP.Instructions				= "Click to slash"
-SWEP.Contact					= ""
-SWEP.Purpose					= "SCP-076 Katana"
-SWEP.Category					= "SCP Weapons"
-SWEP.Spawnable					= true
-SWEP.AdminSpawnable				= true
-SWEP.Thinkdelay       		    = 0
-SWEP.Primary.Delay				= 0.25
-SWEP.Primary.Recoil				= 0
-SWEP.Primary.Damage				= math.random(28,45)
-SWEP.Primary.NumShots			= 1
-SWEP.Primary.Cone				= 0
-SWEP.Primary.ClipSize			= -1
-SWEP.ISSCP 						= true
-SWEP.Primary.DefaultClip		= -1
-SWEP.Primary.Automatic   		= false
-SWEP.Primary.Ammo         		= "none"
-SWEP.Cloaked                    = false
-SWEP.CSMuzzleFlashes			= true
-SWEP.Secondary.Delay			= 5
-SWEP.Secondary.Recoil			= 0
-SWEP.Secondary.Damage			= 2
-SWEP.Secondary.NumShots			= 1
-SWEP.Secondary.Cone				= 0
-SWEP.Secondary.ClipSize			= -1
-SWEP.Secondary.DefaultClip		= -1
-SWEP.Secondary.Automatic  	 	= false
-SWEP.Primary.Sound				= Sound( "Weapon_Knife.Slash" )
-SWEP.HitDistance				= 75
-SWEP.HitInclination				= 0.4
-SWEP.HitPushback				= 850
-SWEP.HoldType 					= "melee2"
-SWEP.ViewModelFOV				= 60
-SWEP.ViewModelFlip				= false
-SWEP.ViewModel					= "models/weapons/v_katana.mdl"
-SWEP.WorldModel 				= "models/weapons/w_katana.mdl"
-SWEP.ThrowTimer    		        = false
-SWEP.MeleeRevert    	        = false
-SWEP.droppable 					= false
-SWEP.IronSightsPos				= Vector (0, 0, 0)
-SWEP.IronSightsAng 				= Vector (-66.4823, 4.1536, 0)
+SWEP.ViewModel = "models/weapons/tfa_l4d2/c_kf2_katana.mdl"
+SWEP.ViewModelFOV = 65
+SWEP.VMPos = Vector(0,-3,-1)
+SWEP.UseHands = true
+SWEP.CameraOffset = Angle(0,-2,0)
+SWEP.droppable = false
 
-function SWEP:Initialize()
-	self:SetWeaponHoldType("melee2");
-	self.mode = 1
-	self.changemode = 0
-	self.CanPrimary = true
-	self.Weapon:SetNetworkedBool( "Ironsights", false )
+SWEP.InspectPos = Vector(17.184, -4.891, -11.652) - SWEP.VMPos
+SWEP.InspectAng = Vector(70, 46.431, 70)
 
-	self.Hit = {
-		Sound( "weapons/rpg/shotdown.wav" )
-	};
-	self.FleshHit = {
-  		Sound( "ambient/machines/slicer1.wav" ),
-  		Sound( "ambient/machines/slicer2.wav" ),
-		Sound( "ambient/machines/slicer3.wav" ),
-  		Sound( "ambient/machines/slicer4.wav" ),
-	} ;
-end
+SWEP.WorldModel = "models/weapons/tfa_l4d2/w_kf2_katana.mdl"
+SWEP.HoldType = "melee2"
 
-function SWEP:StatIncrease()
-	timer.Simple(0.5, function()
-  		if not IsValid(self) or not IsValid(self.Owner) then return end
-			if self.Owner:Health() <= 100 then
-	 			self.Owner:SetHealth(self.Owner:Health() + 200)
-	 		end
-  		end)
-end
+SWEP.Primary.Directional = true
 
-function SWEP:Holster()
-	if self:IsValid() and self.Owner:IsValid() then
-		self.Owner:SetJumpPower(200)
-		--[[
-		self:SetNWBool( "Katana_View_Cloak", false)
-		if self:GetNWBool("Katana_Cloak") == true then
-			if not self.Owner:Alive() then
-				self.Owner:SetMaterial("")
-		 		if SERVER then
-					self.Owner:DrawWorldModel( true )
-		  		end
-			else
-				timer.Destroy("CloakTime")
-				self:SetNWBool( "Katana_Cloak", false )
-				self:EmitSound("npc/dog/dog_idle1.wav")
-				self.Owner:DrawShadow( true )
-				self.Owner:SetMaterial("")
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
 
-				if SERVER then
-					self.Owner:DrawWorldModel( true )
-					self.Owner:SetNoTarget(false)
-				end
-			end
-		end
-		]]--
-	end
-end
+SWEP.DisableIdleAnimations = false
 
---[[  ???
-	return true
-end
-]]--
+SWEP.ISSCP = true
 
-function SWEP:OnRemove()
+SWEP.Primary.Attacks = {
+	{
+		['act'] = ACT_VM_HITLEFT, -- Animation; ACT_VM_THINGY, ideally something unique per-sequence
+		['len'] = 16*4.8, -- Trace distance
+		['dir'] = Vector(-65,0,0), -- Trace arc cast
+		['dmg'] = 120, --Damage
+		['dmgtype'] = DMG_SLASH, --DMG_SLASH,DMG_CRUSH, etc.
+		['delay'] = 0.4, --Delay
+		['spr'] = true, --Allow attack while sprinting?
+		['snd'] = "TFA_L4D2_KF2_KATANA.Swing", -- Sound ID
+		['snd_delay'] = 0.26,
+		["viewpunch"] = Angle(1,-5,0), --viewpunch angle
+		['end'] = 0.8, --time before next attack
+		['hull'] = 16, --Hullsize
+		['direction'] = "L", --Swing dir,
+		['hitflesh'] = "TFA_L4D2_KF2_KATANA.HitFlesh",
+		['hitworld'] = "TFA_L4D2_KF2_KATANA.HitWorld"
+	},
+	{
+		['act'] = ACT_VM_MISSLEFT, -- Animation; ACT_VM_THINGY, ideally something unique per-sequence
+		['len'] = 16*4.8, -- Trace distance
+		['dir'] = Vector(-65,0,0), -- Trace arc cast
+		['dmg'] = 120, --Damage
+		['dmgtype'] = DMG_SLASH, --DMG_SLASH,DMG_CRUSH, etc.
+		['delay'] = 0.45, --Delay
+		['spr'] = true, --Allow attack while sprinting?
+		['snd'] = "TFA_L4D2_KF2_KATANA.Swing", -- Sound ID
+		['snd_delay'] = 0.35,
+		["viewpunch"] = Angle(1,-5,0), --viewpunch angle
+		['end'] = 1.2, --time before next attack
+		['hull'] = 16, --Hullsize
+		['direction'] = "L", --Swing dir
+		['hitflesh'] = "TFA_L4D2_KF2_KATANA.HitFlesh",
+		['hitworld'] = "TFA_L4D2_KF2_KATANA.HitWorld"
+	},
+	{
+		['act'] = ACT_VM_HITRIGHT, -- Animation; ACT_VM_THINGY, ideally something unique per-sequence
+		['len'] = 16*4.8, -- Trace distance
+		['dir'] = Vector(65,0,0), -- Trace arc cast
+		['dmg'] = 120, --Damage
+		['dmgtype'] = DMG_SLASH, --DMG_SLASH,DMG_CRUSH, etc.
+		['delay'] = 0.4, --Delay
+		['spr'] = true, --Allow attack while sprinting?
+		['snd'] = "TFA_L4D2_KF2_KATANA.Swing", -- Sound ID
+		['snd_delay'] = 0.35,
+		["viewpunch"] = Angle(1,5,0), --viewpunch angle
+		['end'] = 1.2, --time before next attack
+		['hull'] = 16, --Hullsize
+		['direction'] = "R", --Swing dir
+		['hitflesh'] = "TFA_L4D2_KF2_KATANA.HitFlesh",
+		['hitworld'] = "TFA_L4D2_KF2_KATANA.HitWorld"
+	},
+	{
+		['act'] = ACT_VM_MISSRIGHT, -- Animation; ACT_VM_THINGY, ideally something unique per-sequence
+		['len'] = 16*4.8, -- Trace distance
+		['dir'] = Vector(65,0,0), -- Trace arc cast
+		['dmg'] = 120, --Damage
+		['dmgtype'] = DMG_SLASH, --DMG_SLASH,DMG_CRUSH, etc.
+		['delay'] = 0.45, --Delay
+		['spr'] = true, --Allow attack while sprinting?
+		['snd'] = "TFA_L4D2_KF2_KATANA.Swing", -- Sound ID
+		['snd_delay'] = 0.26,
+		["viewpunch"] = Angle(1,5,0), --viewpunch angle
+		['end'] = 1.2, --time before next attack
+		['hull'] = 16, --Hullsize
+		['direction'] = "R", --Swing dir
+		['hitflesh'] = "TFA_L4D2_KF2_KATANA.HitFlesh",
+		['hitworld'] = "TFA_L4D2_KF2_KATANA.HitWorld"
+	}
+}
 
-end
+SWEP.Secondary.Attacks = {}
+
+
+SWEP.Secondary.CanBash = true
+SWEP.Secondary.BashDamage = 50
 
 function SWEP:Deploy()
 	self.Weapon:SendWeaponAnim(ACT_VM_DRAW)
@@ -133,163 +120,3 @@ function SWEP:Deploy()
 
 	return true
 end
-
-if SERVER then
-	function SWEP:Equip( NewOwner )
-		self:StatIncrease()
-		--[[
-		self:SetNWBool( "KatanaJump", false )
-		self:SetNWBool( "Katana_View_Cloak", false )
-		self:SetNWBool( "Katana_Cloak", false )
-		]]--
-	end
-end
-
-function SWEP:SecondaryAttack()
-
-end
-
-function SWEP:PrimaryAttack()
-	if not IsValid(self) or not IsValid(self.Owner) then return end
-	self.Weapon:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
-	self.Weapon:EmitSound( self.Primary.Sound )	--slash in the wind sound here
-	--[[
-	timer.Simple(0.2, function()
-		if IsValid(self) then
-			self:Slash()
-		end
-	end)
-	]]--
-	self:Slash()
-	self.Weapon:SendWeaponAnim( ACT_VM_MISSCENTER )
-	self.Owner:SetAnimation( PLAYER_ATTACK1 )
-end
-
-function SWEP:Slash()
-  local trace = self.Owner:GetEyeTrace();
-  if trace.HitPos:Distance(self.Owner:GetShootPos()) <= 114 then
-    if not IsValid(self) or not IsValid(self.Owner) then return end
-    pos = self.Owner:GetShootPos()
-    ang = self.Owner:GetAimVector()
-    damagedice = math.Rand(0.9,1.50)
-    pain = self.Primary.Damage * damagedice
-    if SERVER and IsValid(self.Owner) then
-      local slash = {}
-      slash.start = pos
-      slash.endpos = pos + (ang * 70)
-      slash.filter = self.Owner
-      slash.mins = Vector(1,1,1) * -20
-      slash.maxs = Vector(1,1,1) * 20
-      local slashtrace = util.TraceHull(slash)
-      if slashtrace.Hit then
-        targ = slashtrace.Entity
-        if targ:IsPlayer() or targ:IsNPC() then
-          if targ:Team() == TEAM_SCP then
-            return
-          end
-          self.Owner:EmitSound( self.FleshHit[math.random(1,#self.FleshHit)] );
-          paininfo = DamageInfo()
-          paininfo:SetDamage(pain)
-          paininfo:SetDamageType(DMG_SLASH)
-          paininfo:SetAttacker(self.Owner)
-          paininfo:SetInflictor(self.Weapon)
-          local RandomForce = math.random(1000,20000)
-          paininfo:SetDamageForce(slashtrace.Normal * RandomForce)
-          if targ:IsPlayer() then
-            targ:ViewPunch( Angle( -10, -20, 0 ) )
-          end
-          if SERVER then
-            local blood = targ:GetBloodColor()
-            local fleshimpact = EffectData()
-            fleshimpact:SetEntity(self.Weapon)
-            fleshimpact:SetOrigin(slashtrace.HitPos)
-            fleshimpact:SetNormal(slashtrace.HitPos)
-            if blood >= 0 then
-              fleshimpact:SetColor(blood)
-              util.Effect("BloodImpact", fleshimpact)
-            end
-          end
-          if SERVER then targ:TakeDamageInfo(paininfo) end
-		else
-			--print("==Trace==")
-			local tr = self.Owner:GetEyeTraceNoCursor()
-			if tr.Entity:IsValid() then
-				if tr.Entity:GetClass() == "func_breakable" then
-					--Get the distance
-	          		self.Owner:EmitSound( self.Hit[math.random(1,#self.Hit)] )
-					tr.Entity:TakeDamage( 100, self.Owner, self.Owner )
-				elseif tr.Entity:GetClass() == 'prop_dynamic' then
-					if string.lower(tr.Entity:GetModel()) == 'models/foundation/containment/door01.mdl' then
-						if not preparing then
-							tr.Entity:TakeDamage( 26, self.Owner, self.Owner ) --Just no damage for now ok thx 
-						end
-						tr.Entity:EmitSound(Sound('MetalGrate.BulletImpact'))
-					end
-				else
-		            look = self.Owner:GetEyeTraceNoCursor()
-		            util.Decal("ManhackCut", look.HitPos + look.HitNormal, look.HitPos - look.HitNormal )
-				end
-			else
-	            look = self.Owner:GetEyeTrace()
-	            util.Decal("ManhackCut", look.HitPos + look.HitNormal, look.HitPos - look.HitNormal )
-			end
-			--print("==DONE!==")
-          self.Owner:EmitSound( self.Hit[math.random(1,#self.Hit)] )
-        end
-      end
-    end
-    if( trace.Entity:IsPlayer() or trace.Entity:IsNPC() or trace.Entity:GetClass()=="prop_ragdoll" ) then
-      if trace.Entity:IsPlayer() then
-        if trace.Entity:Team() == TEAM_SCP then
-          --Don't Shoot them!
-          return -- We cannot slash them, they're our friend
-        end
-      end
-    end
-  end
-end
-
------------------------------------Credit for this part goes to RobotBoy655, Just rewritten so I can learn it for myself and altered to my liking-----------------------------------
-
------------------------------------------------------------------------------------------------------------------------------------------------
-
---[[
-function SWEP:Think()
-
-
-	if not IsValid(self) or not IsValid(self.Owner) then return end
-
-	if self:GetNWBool("GrenadeHoldType") == true then
-		self:SetWeaponHoldType("grenade")
-	end
-
-	if self:GetNWBool("GrenadeHoldType") == false then
-		self:SetWeaponHoldType("melee2")
-	end
-
-	if self:GetNWBool("KatanaJump") == true then
-		self:SetWeaponHoldType("melee")
-	end
-
-	if self:GetNWBool("Katana_View_Cloak") == true then
-		self.Owner:GetViewModel():SetMaterial("sprites/heatwave")
-		 else
-		self.Owner:GetViewModel():SetMaterial("")
-	end
-
-	self.Owner:RemoveAllDecals()
-
-
-	if not self.Owner:OnGround() then
-	 self:SetNWBool( "KatanaJump", true )
-	else
-	 self:SetNWBool( "KatanaJump", false )
-	end
-
-	if self.Idle and CurTime()>=self.Idle then
-		self.Idle = nil
-		self.Weapon:SendWeaponAnim( ACT_VM_IDLE )
-	end
-
-end
-]]--
