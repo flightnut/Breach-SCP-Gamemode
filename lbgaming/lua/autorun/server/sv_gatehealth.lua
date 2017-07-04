f45adc233d85d1603c7f8d1459d9648b0fcf08b9
Link2006 <alloatous2@hotmail.com>
2017-07-04 19:18:26 -0400

    Updated link2006dmg and Gatehealth in an attempt to fix Null Entity spam

--- lbgaming/lua/autorun/server/sv_gatehealth.lua
+++ lbgaming/lua/autorun/server/sv_gatehealth.lua
@@ -19,28 +19,39 @@ hook.Add('InitPostEntity','GateHealth.Init',GateHealth_Init)
 
 --Hook the entity damages...
 hook.Add('EntityTakeDamage','GateHealth.EntityTakeDamage',function(prop,dmginfo)
-	if preparing then 
-		if dmginfo then 
-			if IsValid(dmginfo:GetAttacker()) then 
-				if dmginfo:GetAttacker():IsPlayer() then 
+	if preparing then
+		if dmginfo then
+			if IsValid(dmginfo:GetAttacker()) then
+				if dmginfo:GetAttacker():IsPlayer() then
 					dmginfo:GetAttacker():PrintMessage(HUD_PRINTCENTER,"Please wait for the round to start.")
-				end 
-			end 
-		end 
-		return true --Fuck off katana :( 
-	end 
-	
+				end
+			end
+		end
+		return true --Fuck off katana :(
+	end
+
     if prop:GetClass() == 'prop_dynamic' then
         if string.lower(prop:GetModel()) == 'models/foundation/containment/door01.mdl' then
             --The model seems to match a gate a model
             if IsValid(prop:GetParent()) then --Does it have a parent?
                 if prop:Health() >= 0 then
                     if prop:GetParent():GetClass() == 'func_door' then --Is it a door?
-						if dmginfo:GetAttacker():GetActiveWeapon():GetClass() == "tfa_l4d2_kfkat" then -- new weapon then 
-							prop:SetHealth(prop:Health() - math.random(1,5)) --Lower this by that much health
-						else
-							prop:SetHealth(prop:Health() - dmginfo:GetDamage()) --Lower this by that much health
-						end
+                        --print(dmginfo:GetAttacker())
+                        if IsValid(dmginfo:GetAttacker()) then
+                            --print(dmginfo:GetAttacker():GetActiveWeapon())
+                            if IsValid(dmginfo:GetAttacker():GetActiveWeapon()) then
+                                --print(dmginfo:GetAttacker():GetActiveWeapon():GetClass())
+                                if dmginfo:GetAttacker():GetActiveWeapon():GetClass() == "tfa_l4d2_kfkat" then -- new weapon then
+                                    prop:SetHealth(prop:Health() - math.random(1,5)) --Lower this by that much health
+                                else
+                                    prop:SetHealth(prop:Health() - dmginfo:GetDamage()) --Lower this by that much health
+                                end
+                            else
+                                prop:SetHealth(prop:Health() - dmginfo:GetDamage()) --Lower this by that much health
+                            end
+                        else
+                            prop:SetHealth(prop:Health() - dmginfo:GetDamage()) --Lower this by that much health
+                        end
                         if prop:Health() <= 0 then --Yes it's Gate A or B, Destroy it.
                             prop:EmitSound(Sound('Metal_Box.Break'))
                             prop:GetParent():Fire('Open') --Force it to open.
--- lbgaming/lua/autorun/sh_link2006dmg.lua
+++ lbgaming/lua/autorun/sh_link2006dmg.lua
@@ -1,14 +1,20 @@
 print("[sh_link2006dmg] Starting ScalePlayerDamage hook...")
 hook.Add("ScalePlayerDamage","Link2006_ScaleDamage",function(ply,hitgroup,dmginfo)
-    if dmginfo and dmginfo:GetAttacker() and dmginfo:GetAttacker():GetActiveWeapon() and dmginfo:GetAttacker():GetClass() then
-        if ply:Team() ~= TEAM_SCP then --
-            if dmginfo:GetAttacker():GetActiveWeapon():GetClass() == "weapon_stunstick" then
-                dmginfo:ScaleDamage(0.33) --66% Damage nerf
-            elseif dmginfo:GetAttacker():GetActiveWeapon():GetClass() == "weapon_crowbar" then
-                dmginfo:ScaleDamage(0.35)
-            end
-		end
-    end
+	if IsValid(dmginfo) then 
+		if IsValid(dmginfo:GetAttacker()) then 
+			if IsValid(dmginfo:GetAttacker():GetActiveWeapon()) then 
+				if dmginfo and dmginfo:GetAttacker() and dmginfo:GetAttacker():GetActiveWeapon() and dmginfo:GetAttacker():GetClass() then
+					if ply:Team() ~= TEAM_SCP then --
+						if dmginfo:GetAttacker():GetActiveWeapon():GetClass() == "weapon_stunstick" then
+							dmginfo:ScaleDamage(0.33) --66% Damage nerf
+						elseif dmginfo:GetAttacker():GetActiveWeapon():GetClass() == "weapon_crowbar" then
+							dmginfo:ScaleDamage(0.35)
+						end
+					end
+				end
+			end
+		end 
+	end 
     --Don't block the damage on our hook.
 
 	--Removes knockback on everyone (!)
